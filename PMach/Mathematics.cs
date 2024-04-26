3f0dae67d50e58db2c6ca1e46037e99c1f4d8da5
Chaos <91404177+XSonic2X@users.noreply.github.com>
2024-04-26 22:45:42 +0300

    PMach

---
+++ PMach/Mathematics.cs
@@ -0,0 +1,56 @@
+﻿using System.Numerics;
+
+namespace PMach
+{
+    static public class Mathematics
+    {
+
+        /// <summary>
+        /// Вычисление периметра круга если знаем радиус.
+        /// </summary>
+        /// <param name="Radius"></param>
+        /// <returns></returns>
+        static public double AreaCircle(double Radius) => Math.PI * Math.Pow(Radius, 2);
+
+        /// <summary>
+        /// Вычисление периметра треугольника  если знаем длину сторон.
+        /// </summary>
+        /// <param name="lengthA"></param>
+        /// <param name="lengthB"></param>
+        /// <param name="lengthC"></param>
+        /// <returns></returns>
+        static public double AreaTriangle(double lengthA, double lengthB, double lengthC)
+        {
+            double p = (lengthA + lengthB + lengthC) / 2;
+            return Math.Sqrt(p * (p - lengthA) * (p - lengthB) * (p - lengthC));
+        }
+
+        /// <summary>
+        /// Вычисление периметра круга от центра окружности на координатной плоскостью.
+        /// </summary>
+        /// <param name="vector1"></param>
+        /// <param name="vector2"></param>
+        /// <returns></returns>
+        static public double AreaCircle(Vector2 vector1, Vector2 vector2)
+            => AreaCircle(Distance(vector1, vector2));
+
+        /// <summary>
+        /// Вычисление периметра треугольника если знаем углы на координатной плоскостью.
+        /// </summary>
+        /// <param name="vectorA"></param>
+        /// <param name="vectorB"></param>
+        /// <param name="vectorC"></param>
+        /// <returns></returns>
+        static public double AreaTriangle(Vector2 vectorA, Vector2 vectorB, Vector2 vectorC)
+            => AreaTriangle(Distance(vectorA, vectorB), Distance(vectorB, vectorC), Distance(vectorC, vectorA));
+
+        /// <summary>
+        /// Вычисление расстояния. 
+        /// </summary>
+        /// <param name="vector1"></param>
+        /// <param name="vector2"></param>
+        /// <returns></returns>
+        static private double Distance(Vector2 vector1, Vector2 vector2)
+            => Math.Sqrt( Math.Pow(vector1.X - vector2.X, 2) + Math.Pow(vector1.Y + vector2.Y, 2));
+    }
+}
---
+++ TestProjectMath/UnitTestPMach.cs
@@ -0,0 +1,20 @@
+using PMach;
+
+namespace TestProjectMath
+{
+    [TestClass]
+    public class UnitTestPMach
+    {
+        [TestMethod]
+        public void TestAreaTriangle()
+        {
+            double p = Mathematics.AreaTriangle(10, 6, 8);
+            Assert.AreEqual(24, p);
+        }
+        [TestMethod]
+        public void TestAreaCircle() 
+        {
+            Assert.AreEqual(31, Math.Truncate(Mathematics.AreaCircle(3.142)));
+        }
+    }
+}
